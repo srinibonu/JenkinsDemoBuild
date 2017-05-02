745b0745cfc810a7fcc62aae0f9e7984e05b800d
srini bonu <bonusrao@gmail.com>
2017-05-02 16:41:34 -0400

    First Commit

---
+++ src/test/java/firstjenkinsbuild/FirstTestFrmJenkins.java
@@ -0,0 +1,14 @@
+package firstjenkinsbuild;
+
+import org.testng.annotations.Test;
+
+public class FirstTestFrmJenkins {
+	
+	@Test
+	public void helloJenkins(){
+		
+		System.out.print("First Project to be built from Jenkins\n");
+		System.out.print("Hello World\n");
+	}
+
+}
