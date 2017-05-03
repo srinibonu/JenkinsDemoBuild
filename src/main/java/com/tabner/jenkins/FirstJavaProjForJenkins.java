4bc4121f0746290d071c2bbf4c7a92545f12c980
srini bonu <bonusrao@gmail.com>
2017-05-03 10:49:45 -0400

    Added java source

---
+++ src/main/java/com/tabner/jenkins/FirstJavaProjForJenkins.java
@@ -0,0 +1,14 @@
+package com.tabner.jenkins;
+
+public class FirstJavaProjForJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Jenkins \n ");
+		System.out.println("Building Maven Project from Jenkins \n ");
+		
+		
+	}
+
+}
